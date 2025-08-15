cc1727c984243a1072633a89467f445f87738066
Sardor aka <sardor@example.com>
2025-08-15 18:13:58 +0500

    2-masala

---
+++ 2.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')()
+var ism = prompt("Ismingizni kiriting: ")
+var familiya = prompt("Familiyangizni kiriting: ")
+ismFamiliya = ism + " " + familiya
+console.log(ismFamiliya)
